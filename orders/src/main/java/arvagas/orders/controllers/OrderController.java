448d9143b8cdcb6bc1306ef3671280a542ec2ead
arvagas <arvagas@gmail.com>
2021-08-29 15:31:55 -0400

    added put endpoint for orders

--- orders/src/main/java/arvagas/orders/controllers/OrderController.java
+++ orders/src/main/java/arvagas/orders/controllers/OrderController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -55,5 +56,14 @@ public class OrderController {
     responseHeaders.setLocation(newRestaurantURI);
 
     return new ResponseEntity<>(newOrder, responseHeaders, HttpStatus.CREATED);
+  }
+
+  @PutMapping(value = "/order/{id}", produces = "application/json", consumes = "application/json")
+  public ResponseEntity<?> replaceOrderById(@PathVariable long id, @RequestBody @Valid Order order) {
+    order.setOrdnum(id);
+
+    Order replaceOrder = orderServices.save(order);
+
+    return new ResponseEntity<>(replaceOrder, HttpStatus.OK);
   }
 }
