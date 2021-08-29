746edba608cc488ca639157bd67fab4b1fba7d8e
arvagas <arvagas@gmail.com>
2021-08-29 14:28:10 -0400

    created put endpoint for customers

--- orders/src/main/java/arvagas/orders/controllers/CustomerController.java
+++ orders/src/main/java/arvagas/orders/controllers/CustomerController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -73,4 +74,12 @@ public class CustomerController {
 
     return new ResponseEntity<>(newCustomer, responseHeaders, HttpStatus.OK);
   }
-}
+
+  @PutMapping(value = "/customer/{id}", produces = "application/json", consumes = "application/json")
+  public ResponseEntity<?> replaceCustomerById(@PathVariable long id, @Valid Customer customer) {
+    customer.setCustcode(id);
+
+    Customer replaceCustomer = customerServices.save(customer);
+
+    return new ResponseEntity<>(replaceCustomer, HttpStatus.OK);
+  }
